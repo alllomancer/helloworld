3991c1161fa676c4f426e4116a4bf5f1acb9b92a
alllomancer <54928349+alllomancer@users.noreply.github.com>
2020-06-25 16:59:38 +0300

    Created an hello world web server

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
-
-func helloworld() string {
-	return "Hello World!!"
-}
+import (
+    "fmt"
+    "net/http"
+)
 
 func main() {
-	fmt.Println(helloworld())
+    http.HandleFunc("/", HelloServer)
+    http.ListenAndServe(":8080", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
