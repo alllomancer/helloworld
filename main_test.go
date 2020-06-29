8c466c6f05c67d1fd5ddc23a04dad1161aa4b8bf
Dan Yelovitch <dany@ziprecruiter.com>
2020-06-29 15:39:40 +0300

    changed to cookie monster

--- main_test.go
+++ main_test.go
@@ -29,7 +29,7 @@ func TestHealthCheckHandler(t *testing.T) {
 	}
 
 	// Check the response body is what we expect.
-	expected := `Hello, test!`
+	expected := `Hello, cookie monster!`
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
